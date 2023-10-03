f9dab197e6d282222b7f7ff9ce21ab5839166a8a
DWprog <dawid.window@gmail.com>
2023-10-03 11:36:12 +0200

    Zadanie dzień 4.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,50 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            int sex; //1=mężczyzna, 0=kobieta
+            int age;
+            
+            int ageLimit = 30;
+            int ageAdult = 18;
+
+            name = "Dorota";
+            sex = 0;
+            age = 37;
+
+            if (sex==1) //mężczyzna
+            {
+                if (age<ageAdult)
+                {
+                    Console.WriteLine("Niepełnoletni mężczyzna");
+                }
+
+                else if (age<ageLimit)
+                {
+                    Console.WriteLine($"Mężczyzna poniżej {ageLimit} lat");
+                }
+                else
+                {
+                    Console.WriteLine($"{name}, lat {age}");
+                }
+            }
+
+            if (sex == 0) //kobieta
+            {
+                if (age < ageAdult)
+                {
+                    Console.WriteLine("Niepełnoletnia kobieta");
+                }
+
+                else if (age < ageLimit)
+                {
+                    Console.WriteLine($"Kobieta poniżej {ageLimit} lat");
+                }
+                else
+                {
+                    Console.WriteLine($"{name}, lat {age}");
+                }
+            }
         }
     }
 }
